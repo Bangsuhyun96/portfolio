4076e3bf5f50d0984647237b60a11ca59ae29790
Bangsuhyun96 <82694273+Bangsuhyun96@users.noreply.github.com>
2021-04-17 14:00:17 +0900

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     $('.headline_icon').click(function(e){
+// 		e.preventDefault();
+// 		var scrolltarget = $($(this).attr("data-target")).offset().top;
+// 		$('html').animate({
+// 			scrollTop: scrolltarget
+// 		}, 700);
+// 	});
+//     $('.top').click(function(e){
+// 		e.preventDefault();
+// 		var scrolltarget = $($(this).attr("data-target")).offset().top;
+// 		$('html').animate({
+// 			scrollTop: scrolltarget
+// 		}, 700);
+// 	});
+// });
